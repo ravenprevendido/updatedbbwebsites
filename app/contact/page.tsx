4f0f85cf19020e35333b545c27752cbfce203b04
Raven Prevendido <prevendidoraven_bsit@plmun.edu.ph>
2025-09-30 01:57:40 -0700

    upddtedbbsite5.2

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,17 +1,17 @@
 "use client"
-import React, { Suspense } from 'react'
+import React, { Suspense, useState } from 'react'
 import { Header } from '../components'
 import ContactPages from '../components/ContactPages'
 import Footer from '../components/Footer'
 
 const page = () => {
-  
+  const [searchValue, setSearchValue] = useState("");
+    const [selectedServiceFromHeader, setSelectedServiceFromHeader] = useState<string | null>(null);
+        
   return (
     <div>
       <Suspense fallback={<></>}>
-        <Header searchValue={''} setSearchValue={function (value: React.SetStateAction<string>): void {
-              throw new Error('Function not implemented.')
-          } }/>
+        <Header searchValue={searchValue} setSearchValue={setSearchValue} setSelectedServiceFromHeader={setSelectedServiceFromHeader}/>
           </Suspense>
           <ContactPages/>
           <Footer/>
